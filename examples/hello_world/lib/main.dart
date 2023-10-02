ca7cd0986c850606029759a626032f6a5319da35
Lorenzo Bervian <lorenzolbm32@gmail.com>
2023-10-02 19:33:52 -0300

    teeste

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -1,16 +1,23 @@
 // Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-import 'package:flutter/widgets.dart';
+void main() => runApp(const AppWidget());
 
-void main() =>
-  runApp(
-    const Center(
-      child:
-        Text('Hello, world!',
-          key: Key('title'),
-          textDirection: TextDirection.ltr,
-        ),
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
+        style:
+            TextStyle(color: Color.fromARGB(255, 0, 237, 245), fontSize: 100.0),
       ),
     );
+  }
+}
